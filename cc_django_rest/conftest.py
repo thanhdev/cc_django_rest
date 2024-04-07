d96503aea5eb7eaa840090f57053fd5be956c4eb
tthanh <ytominhthanh@gmail.com>
2024-04-07 12:20:40 +0700

    Init

---
+++ cc_django_rest/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cc_django_rest.users.models import User
+from cc_django_rest.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
---
+++ cc_django_rest/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from cc_django_rest.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
