697007e56e353df820af22d5cc5393b9b5d93cfb
tthanh <ytominhthanh@gmail.com>
2024-04-11 14:46:11 +0700

    Frontend - Try POST API call

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,33 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HttpClient} from "@angular/common/http";
-import { environment } from './../environments/environment';
+import { environment } from '../environments/environment';
+import {getCookie} from "./utils";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular';
   name = '';
+  newName = '';
   userCount = 0;
 
   constructor(private http: HttpClient) {
-    this.fetchData();
-    console.log(environment.apiUrl)
+    this.loadUserInfo();
   }
 
-  fetchData() {
-    this.http.get<any>('/api/users/me/').subscribe(data => {
+  loadUserInfo() {
+    const options = { withCredentials: true };
+
+    this.http.get<any>(environment.apiUrl + '/users/me/', options).subscribe(data => {
       this.name = data.name;
     });
 
-    let listUrl = '/api/users/';
+    let listUrl = environment.apiUrl + '/users/';
     this.http.get<any>(listUrl).subscribe(data => {
       this.userCount = data.length;
+    });
+  }
+
+  updateUserName() {
+    const csrfToken = getCookie('csrftoken') || '';
+    const options = { withCredentials: true, headers: { 'X-CSRFToken': csrfToken } };
+
+    this.http.patch<any>(environment.apiUrl + '/users/root/', { name: this.newName }, options).subscribe(() => {
+      this.loadUserInfo()
     });
   }
 
